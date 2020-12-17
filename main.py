5890a7d6ba30830d0b318e8f2811a923b8a7f695
JailsonVS <jailsonviana0009@gmail.com>
2020-12-17 18:26:28 +0000

    Exercicio OK

--- main.py
+++ main.py
@@ -4,7 +4,18 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    N1 = float(input("Digite o primeiro número \n"))
+    N2 = float(input("Digite o segundo número \n"))
+    N3 = float(input("Digite o terceiro número \n"))
 
+    if N1 >= N2 and N1 >= N3:
+      maior = N1
+    elif N2 >= N1 and N2 >= N3:
+      maior = N2
+    else:
+      maior = N3
+    
+    print(f"{maior}")
 
 if __name__ == '__main__':
     main()
